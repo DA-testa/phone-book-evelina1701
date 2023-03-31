dfa74c55c13ed27b22325f810da59c48c8bca416
Evelīna Geikina <118215390+evelina1701@users.noreply.github.com>
2023-03-31 16:10:47 +0000

    test

--- main.py
+++ main.py
@@ -8,6 +8,55 @@ class Query:
         self.b_count = 1000
         self.b = [[] for _ in range(self.b_count)]
 
+    def hashFunction(self,text):
+        reply = 0
+        for i in reversed(text):
+            reply = (reply * self.multiplier + ord(i)) % self.prime
+        return reply % self.b_count
+
+    def add(self,num,name):
+        hashedNum = self.hashFunction(str(num))
+        bucket = self.b[hashedNum]
+        for j in range(len(bucket)):
+            if bucket[j][0] == num:
+                bucket[j] = (num,name)
+                return
+        bucket.append((num,name))
+
+    def delete(self,num):
+        hashedNum = self.hashFunction(str(num))
+        bucket = self.b[hashedNum]
+        for j in range(len(bucket)):
+            if bucket[j][0] == num:
+                del bucket[j]
+                return
+
+    def find(self,num):
+        text = "not found"
+        hashedNum = self.hashFunction(str(num))
+        bucket = self.b[hashedNum]
+        for j in range(len(bucket)):
+            if bucket[j][0] == num:
+                return bucket[j][1]
+        return text
+
+if __name__ == '__main__':
+    # write_responses(process_queries(read_queries()))
+    phoneBook = Query()
+    n = int(input())
+    answer = []
+    for i in range(n):
+        query = input().split()
+        if query[0] == "add":
+            phoneBook.add(int(query[1]),query[2])
+        elif query[0] == "del":
+            phoneBook.delete(int(query[1]))
+        elif query[0] == "find":
+            answer.append(phoneBook.find(int(query[1])))
+
+    for j in range(len(answer)):
+        print(answer[j])
+
 # def read_queries():
 #     n = int(input())
 #     return [Query(input().split()) for i in range(n)]
@@ -42,52 +91,3 @@ class Query:
 #                     break
 #             result.append(response)
 #     return result
-
-    def hashFunction(self,text):
-        reply = 0
-        for i in reversed(text):
-            reply = (reply * self.multiplier + ord(i)) % self.prime
-        return reply % self.b_count
-
-    def add(self,num,name):
-        hashedNum = self.hashFunction(str(num))
-        bucket = self.b[hashedNum]
-        for j in range(len(bucket)):
-            if bucket[j][0] == num:
-                bucket[j] = (num,name)
-                return
-        bucket.append((num,name))
-
-    def delete(self,num):
-        hashedNum = self.hashFunction(str(num))
-        bucket = self.b[hashedNum]
-        for j in range(len(bucket)):
-            if bucket[j][0] == num:
-                del bucket[j]
-                return
-
-    def find(self,num):
-        text = "not found"
-        hashedNum = self.hashFunction(str(num))
-        bucket = self.b[hashedNum]
-        for j in range(len(bucket)):
-            if bucket[j][0] == num:
-                return bucket[j][1]
-            return text
-
-if __name__ == '__main__':
-    # write_responses(process_queries(read_queries()))
-    phoneBook = Query()
-    n = int(input())
-    answer = []
-    for i in range(n):
-        query = input().split()
-        if query[0] == "add":
-            phoneBook.add(int(query[1]),query[2])
-        elif query[0] == "del":
-            phoneBook.delete(int(query[1]))
-        elif query[0] == "find":
-            answer.append(phoneBook.find(int(query[1])))
-
-    for j in range(len(answer)):
-        print(answer[j])

daa47cc933f98be9326d719e68b2ead6d818527e
Evelīna Geikina <118215390+evelina1701@users.noreply.github.com>
2023-04-02 12:09:12 +0000

    test

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
+# Evelīna Geikina 221RDB068 14.grupa
 # python3
 import random
 
 class Query:
     def __init__(self):
+        # Inicializē prime un multiplier ar nejaušiem skaitļiem un inicializē ierakstu skaitu un sarakstu
         self.prime = random.randint(0,10000019)
         self.multiplier = random.randint(0,263)
         self.record_count = 1000
         self.records = [[] for _ in range(self.record_count)]
 
     def hashFunction(self,text):
+        # Lai aprēķinātu ieraksta atrašanās vietu, tiek definēta hash funkcija
         reply = 0
         for i in reversed(text):
             reply = (reply * self.multiplier + ord(i)) % self.prime
         return reply % self.record_count
 
     def add(self,num,name):
+        # Tiek aprēķināta ieraksta atrašanās vieta. Ja tajā vietā jau ir ierakstīta informācija, tad
+        # tā tiek aizvietota ar jaunu ierakstu, pretējā gadījumā tiek izveidots jauns ieraksts
         hashedNum = self.hashFunction(str(num))
         record = self.records[hashedNum]
         for j in range(len(record)):
@@ -24,6 +29,8 @@ class Query:
         record.append((num,name))
 
     def delete(self,num):
+        # Tiek aprēķināta ieraksta atrašanās vieta. Ja tajā vietā ir ierakstīta informācija, tad
+        # tā tiek dzēsta, pretējā gadījumā nekas netiks izdarīts
         hashedNum = self.hashFunction(str(num))
         record = self.records[hashedNum]
         for j in range(len(record)):
@@ -32,6 +39,8 @@ class Query:
                 return
 
     def find(self,num):
+        # Tiek aprēķināta ieraksta atrašanās vieta. Ja tajā vietā ir ierakstīta vajadzīgā informācija, tad
+        # tiks atgriezts personas vārds, pretējā gadījumā tiks izvadīts "not found"
         text = "not found"
         hashedNum = self.hashFunction(str(num))
         record = self.records[hashedNum]
@@ -41,10 +50,13 @@ class Query:
         return text
 
 if __name__ == '__main__':
-    # write_responses(process_queries(read_queries()))
+    # Izveido klases Query objekts
     phoneBook = Query()
+    # Tiek ievadīts komandu skaits
     n = int(input())
+    # Tiek izveidots saraksts metodes 'find' rezultātu saglabāšanai
     answer = []
+    # Tiek apstrādātas visas komandas, kuras tika ievadītas
     for i in range(n):
         query = input().split()
         if query[0] == "add":
@@ -53,7 +65,7 @@ if __name__ == '__main__':
             phoneBook.delete(int(query[1]))
         elif query[0] == "find":
             answer.append(phoneBook.find(int(query[1])))
-
+    # Tiek izvadītas visas metodes 'find' atbildes
     for j in range(len(answer)):
         print(answer[j])
 
@@ -91,3 +103,5 @@ if __name__ == '__main__':
 #                     break
 #             result.append(response)
 #     return result
+
+ # write_responses(process_queries(read_queries()))
